55e721de2244da10bd70f2acca00c60c9363e504
readyForReal <hustlersteve@gmail.com>
2018-11-07 20:33:26 +0200

    third commit

--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@ document.addEventListener('DOMContentLoaded', () => {
 		timer = require('./parts/timer.js'),
 		gallery = require('./parts/gallery.js'),
 		form = require('./parts/form.js');
+		// calc = require('./parts/calc.js');
 
 	modal();
 	tabs();
 	timer();
 	gallery();
 	form();
+	// calc();
 
 });
